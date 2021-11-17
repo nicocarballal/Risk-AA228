4b2a1a0d5b89f2f813b2509bba4f946c0017357e
jmboettcher <54394842+jmboettcher@users.noreply.github.com>
2021-11-16 16:05:11 -0800

    fixed some typos

--- scripts/utils/heuristics.py
+++ scripts/utils/heuristics.py
@@ -4,13 +4,13 @@ def BST_Heuristic(team, risk_map):
     '''
     Calculates the Border Security Threat of all of the countries owned by a team. The BST is the amount of troops in neighboring
     countries (not your team) divided by the amount of troops in your country. The paper, https://project.dke.maastrichtuniversity.nl/games/files/bsc/Hahn_Bsc-paper.pdf, describes the concept of BST in more detail
-    
+
     Input:
         team: type GameTeam
         risk_map: type GameMap
-    
+
     Output:
-        BST_dict: type dictionary { keys --> names of territories; values --> BSR ratings}
+        BST_dict: type dictionary { keys --> names of territories; values --> BST ratings}
     '''
 
     BST_dict = {}
@@ -26,16 +26,16 @@ def BST_Heuristic(team, risk_map):
 def BSR_Heuristic(team, risk_map):
     '''
     Returns normalized version of the Border Securty Threat
-    
+
     Input:
         team: type GameTeam
         risk_map: type GameMap
-    
+
     Output:
         BSR_dict: type dictionary { keys --> names of territories; values --> BSR ratings}
     '''
     BSR_dict = BST_Heuristic(team, risk_map)
     factor=1.0/sum(BSR_dict.itervalues())
     for country in BSR_dict:
-        BSR_dict[country] = BSR_dict[coutnry]*factor
+        BSR_dict[country] = BSR_dict[country]*factor
     return BSR_dict
